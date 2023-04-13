03a4ad54ff99d98ec5a962042fa6d4441f226a73
Víctor Romero <virn2005@gmail.com>
2023-04-13 11:13:29 -0600

    Commit Prueba

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Victo
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Victor Romero");
+
+        int caso1 = 1;
+
+        switch (caso1) {
+            case 1: {
+                System.out.println("virn1");
+            }
+        }
+
+        System.out.println("Me llamo Victor");
+    }
+
+}
